c3fe494c8108733d4afc21d0cf2459a6b151b48f
Kocsen <kocsenc@gmail.com>
2015-07-05 19:41:24 -0700

    NEW SITE initial commit

---
+++ js/contact_me.js
@@ -0,0 +1,97 @@
+$(function () {
+
+  $("input,textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function ($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function ($form, event) {
+      // Prevent spam click and default submit behaviour
+      $("#btnSubmit").attr("disabled", true);
+      event.preventDefault();
+
+      // get values from FORM
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var message = $("textarea#message").val();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+
+      var sendObj = {
+        key: "IXRuiU6xpNR7g-kX-MGeXg",
+        message: {
+          text: message,
+          subject: "Kocsen.com Email",
+          from_email: email,
+          from_name: name,
+          to: [
+            {
+              email: "kocsenc+mandrill@gmail.com",
+              name: "Kocsen",
+              type: "to"
+            }
+          ],
+          headers: {
+            "Reply-To": "kocsen.comsite@noreply.com"
+          }
+        }
+      };
+
+
+      $.ajax({
+        url: "https://mandrillapp.com/api/1.0/messages/send.json",
+        type: "POST",
+        data: sendObj,
+        cache: false,
+        success: function () {
+          onMailSuccess();
+        },
+        error: function () {
+          onMailError(firstName);
+        }
+      })
+    },
+    filter: function () {
+      return $(this).is(":visible");
+    }
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function (e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+// When clicking on Full hide fail/success boxes
+$('#name').focus(function () {
+  $('#success').html('');
+});
+
+function onMailSuccess() {
+// Enable button & show success message
+  $("#btnSubmit").attr("disabled", false);
+  $('#success').html("<div class='alert alert-success'>");
+  $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+    .append("</button>");
+  $('#success > .alert-success')
+    .append("<strong>Your message has been sent. </strong>");
+  $('#success > .alert-success')
+    .append('</div>');
+
+  //clear all fields
+  $('#contactForm').trigger("reset");
+}
+
+function onMailError(firstName) {
+  // Fail message
+  $('#success').html("<div class='alert alert-danger'>");
+  $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+    .append("</button>");
+  $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+  $('#success > .alert-danger').append('</div>');
+  //clear all fields
+  $('#contactForm').trigger("reset");
+}
